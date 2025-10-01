25d64e1c2e0f64f530958663fa61daceefc59005
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-10-01 15:12:00 -0500

    updating code
    pushing code

--- JS/project.js
+++ JS/project.js
@@ -1,12 +1,15 @@
 var numbers = [256, 144, 4, 64, 49, 100, 625, 1, 16]
 var tenOrBelow = [];
+var allRoots = [];
 
 for (var i = 0; i < numbers.length; i++) {
     var root = Math.sqrt(numbers[i]);
-
+    allRoots.push(root);
+    
     if (root <= 10) {
         tenOrBelow.push(root);
     } 
 }
 
-console.log(tenOrBelow)
+console.log("All square roots:", allRoots);
+console.log("Square roots ≤ 10:", tenOrBelow);
